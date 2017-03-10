08c45aede4a5d3290986c0c1cf27189c1495d932
Tom Wilson <tom.wilson@isobar.com>
2017-03-10 16:07:20 +1100

    blah

--- lib/elixir_dropbox.ex
+++ lib/elixir_dropbox.ex
@@ -10,7 +10,7 @@ defmodule ElixirDropbox do
   @upload_url Application.get_env(:elixir_dropbox, :upload_url)
 
   def post(client, url, body \\ "") do
-    headers = json_headers
+    headers = json_headers()
     post_request(client, "#{@base_url}#{url}", body, headers)
   end
 
