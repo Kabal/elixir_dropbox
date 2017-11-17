6f4ae3dd272c5b37dd98650bdab7e8ee0c0df9ae
Tom Wilson <tom.wilson@isobar.com>
2017-11-17 14:28:36 +1100

    Update

--- README.md
+++ README.md
@@ -1,7 +1,76 @@
+[![Build Status](https://travis-ci.org/sger/elixir_dropbox.svg?branch=master)](https://travis-ci.org/sger/elixir_dropbox)
+[![Coverage Status](https://coveralls.io/repos/github/sger/elixir_dropbox/badge.svg?branch=master)](https://coveralls.io/github/sger/elixir_dropbox?branch=master)
+[![Inline docs](http://inch-ci.org/github/sger/elixir_dropbox.svg)](http://inch-ci.org/github/sger/elixir_dropbox)
+
 # ElixirDropbox
 
  Simple Dropbox v2 client for Elixir.
  work in progress
+
+## Features
+
+* file_requests
+  * /create
+  * /get
+  * /list
+  * /update
+* files
+  * /copy_batch
+  * /copy_batch/check
+  * /copy_reference/get
+  * /copy_reference/save
+  * /copy_v2
+  * /create_folder_v2
+  * /delete_batch
+  * /delete_batch/check
+  * /delete_v2
+  * /download
+  * /get_metadata
+  * /get_preview
+  * /get_temporary_link
+  * /get_thumbnail
+  * /get_thumbnail_batch
+  * /list_folder
+  * /list_folder/continue
+  * /list_folder/get_latest_cursor
+  * /list_folder/longpoll
+  * /list_revisions
+  * /move_batch
+  * /move_batch/check
+  * /move_v2
+  * /permanently_delete
+  * /restore
+  * /save_url
+  * /save_url/check_job_status
+  * /search
+  * /upload
+  * /upload_session/append_v2
+  * /upload_session/finish
+  * /upload_session/finish_batch
+  * /upload_session/finish_batch/check
+  * /upload_session/start
+* paper
+  * /docs/archive
+  * /docs/create
+  * /docs/download
+  * /docs/folder_users/list
+  * /docs/folder_users/list/continue
+  * /docs/get_folder_info
+  * /docs/list
+  * /docs/list/continue
+  * /docs/permanently_delete
+  * /docs/sharing_policy/get
+  * /docs/sharing_policy/set
+  * /docs/update
+  * /docs/users/add
+  * /docs/users/list
+  * /docs/users/list/continue
+  * /docs/users/remove
+* users
+  * /get_account
+  * /get_account_batch
+  * /get_current_account
+  * /get_space_usage
 
 ## Usage
 
@@ -26,7 +95,7 @@ If [available in Hex](https://hex.pm/docs/publish), the package can be installed
   1. Add elixir_dropbox to your list of dependencies in `mix.exs`:
 
         def deps do
-          [{:elixir_dropbox, "~> 0.0.4"}]
+          [{:elixir_dropbox, "~> 0.0.7"}]
         end
 
   2. Ensure elixir_dropbox is started before your application:
@@ -44,7 +113,7 @@ $ mix test
 
 ## Documentation
 
-[https://hexdocs.pm/elixir_dropbox/0.0.2](https://hexdocs.pm/elixir_dropbox/0.0.1)
+[https://hexdocs.pm/elixir_dropbox/0.0.7](https://hexdocs.pm/elixir_dropbox/0.0.7)
 
 ## TODO
 - [ ] adding structs for responses
--- lib/elixir_dropbox.ex
+++ lib/elixir_dropbox.ex
@@ -1,17 +1,21 @@
 defmodule ElixirDropbox do
   @moduledoc """
-  ElixirDropbox is a wrapper for Dropbox API V2 
+  ElixirDropbox is a wrapper for Dropbox API V2
   """
   use HTTPoison.Base
 
   @type response :: {any}
 
   @base_url Application.get_env(:elixir_dropbox, :base_url)
-  @upload_url Application.get_env(:elixir_dropbox, :upload_url)
 
   def post(client, url, body \\ "") do
     headers = json_headers()
     post_request(client, "#{@base_url}#{url}", body, headers)
+  end
+
+  def post_url(client, base_url, url, body \\ "") do
+    headers = json_headers()
+    post_request(client, "#{base_url}#{url}", body, headers)
   end
 
   @spec upload_response(HTTPoison.Response.t) :: response
@@ -23,12 +27,12 @@ defmodule ElixirDropbox do
     end
   end
 
-   @spec download_response(HTTPoison.Response.t) :: response
-   def download_response(response) do
-    case response do
-      {:ok, %{body: body, headers: headers, status_code: 200}} ->
-        {:ok, %{file: body, headers: get_header(headers, "dropbox_api_result") |> Poison.decode}}
-      _-> response
+  @spec download_response(HTTPoison.Response.t) :: response
+  def download_response(%HTTPoison.Response{status_code: 200, body: body, headers: headers}), do: %{body: body, headers: headers}
+  def download_response(%HTTPoison.Response{status_code: status_code, body: body }) do
+    cond do
+    status_code in 400..599 ->
+      {{:status_code, status_code}, JSON.decode(body)}
     end
   end
 
@@ -37,27 +41,20 @@ defmodule ElixirDropbox do
     HTTPoison.post!(url, body, headers, [connect_timeout: 1000000, recv_timeout: 1000000, timeout: 1000000]) |> upload_response
   end
 
+  def upload_request(client, base_url, url, data, headers) do
+    post_request(client, "#{base_url}#{url}", {:file, data}, headers)
+  end
+
+  def download_request(client, base_url, url, data, headers) do
+    headers = Map.merge(headers, headers(client))
+    HTTPoison.post!("#{@upload_url}#{url}", data, headers, [connect_timeout: 1000000, recv_timeout: 1000000, timeout: 1000000]) |> download_response  
+  end
+
   def headers(client) do
     %{ "Authorization" => "Bearer #{client.access_token}" }
   end
 
   def json_headers do
     %{ "Content-Type" => "application/json" }
-  end
-
-  def get_header(headers, key) do
-    headers
-    |> Enum.filter(fn({k, _}) -> k == key end)
-    |> hd
-    |> elem(1)
-  end
-  
-  def upload_request(client, url, data, headers) do
-    post_request(client, "#{@upload_url}#{url}", {:file, data}, headers)
-  end
-
-  def download_request(client, url, data, headers) do
-    headers = Map.merge(headers, headers(client))
-    HTTPoison.post!("#{@upload_url}#{url}", data, headers, [connect_timeout: 1000000, recv_timeout: 1000000, timeout: 1000000]) |> download_response
   end
 end
